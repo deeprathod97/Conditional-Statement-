08c8147e847f500144ae94bc78ddfc1bd128f58f
deeprathod97 <deeprathod9797@gmail.com>
2024-04-17 12:26:27 +0530

    if-else

---
+++ if-else.js
@@ -0,0 +1,10 @@
+let z = 7;
+if (z < 5) {
+  console.log("z is less than 5");
+} else if (z === 7) {
+  console.log("z is equal to 7");
+} else {
+  console.log("z is greater than 5 and not equal to 7");
+}
+
+  
---
+++ main.js
@@ -0,0 +1,6 @@
+let y = 3;
+if (y > 5) {
+  console.log("y is greater than 5");
+} else {
+  console.log("y is not greater than 5");
+}
